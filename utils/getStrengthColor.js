3dd5399c02a6ca1c5b7d2fc20135580a2dd15283
Saheb Giri <iamsahebgiri>
2021-03-16 11:15:12 +0530

    fixes password strength and updated rules

--- utils/getStrengthColor.js
+++ utils/getStrengthColor.js
@@ -4,12 +4,12 @@ const getStrengthColor = (password) => {
     let score = checkPasswordStrength(password);
     if (score > 80)
         return "whatsapp.500";
-    if (score > 60)
-        return "messenger.500";
-    if (score >= 30)
+    else if (score > 60)
+        return "yellow.500";
+    else if (score >= 30)
         return "red.500";
-
-    return "yellow.500";
+    else 
+        return "red.600";
 }
 
 export default getStrengthColor;
